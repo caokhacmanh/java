a1b783737835a8fd9dc43df74e59e9e213f472ed
caokhacmanh <caokhacmanh2018@gmail.com>
2021-08-19 11:18:59 +0700

    finis less18

---
+++ Learn/stringMethod.java
@@ -0,0 +1,19 @@
+package Learn;
+import java.util.Scanner;
+
+public class stringMethod {
+    public static void main(String[] args) {
+        String name = "Khacmanh";
+        // boolean result = name.equals("Khacmanh"); // chek if those equals?
+        // int result = name.length(); // return the length if that string
+        // char result = name.charAt(0); // return char at name[n];
+        // int result = name.indexOf("c") // return the index of "c" in the string
+        // boolean result = name.isEmpty(); // chek if empty
+        // String result = name.toUpperCase(); // set result as name's all uppercase
+        // String result = name.toLowerCase(); // set result as name's all lowercase
+        // String result = name.trim(); // remove all the spaces in the string name
+        // String result = name.replace('c', 'z'); // replace c to z
+
+        // System.out.println(result);
+    }
+}
---
+++ Prog/_2DegreeEquation.java
@@ -0,0 +1,18 @@
+package Prog;
+import java.util.Scanner;
+
+public class _2DegreeEquation {
+    public static void main(String[] args) {
+        Scanner jin = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("Input a: ");
+        a = jin.nextInt();
+
+        System.out.print("Input b: ");
+        b = jin.nextInt();
+        
+        System.out.print("Input c: ");
+        c = jin.nextInt();
+    }
+}
