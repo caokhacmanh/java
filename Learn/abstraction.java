af785072b307e890ecb07f3c0d421661cb990100
caokhacmanh <caokhacmanh2018@gmail.com>
2021-08-20 16:09:37 +0700

    .

--- Learn/abstraction.java
+++ Learn/abstraction.java
@@ -1,5 +1,23 @@
 package Learn;
 
 public class abstraction {
+    public static void main(String[] args) {
+        Car3 car = new Car3();
+        car.go();
+    }
+}
+
+abstract class Vehicles2 {
+    abstract void go();
+    
+}
+
+class Car3 extends Vehicles2 {
+
+    @Override
+    void go() {
+        System.out.println("The driver is driving the car");
+        
+    }
     
 }
