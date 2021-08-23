e999e6ba48bd4aada043c3c1db57392bfac1d885
caokhacmanh <caokhacmanh2018@gmail.com>
2021-08-23 18:50:59 +0700

    learnin

--- Learn/copyOpject.java
+++ Learn/copyOpject.java
@@ -3,9 +3,12 @@ package Learn;
 public class copyOpject {
     public static void main(String[] args) {
         Car5 car1 = new Car5("Car1", "Trucks", 2000);
-        Car5 car2 = new Car5("Car2", "Trucks", 2001);
 
-        // car1 = car2; // the address of the car1 is the same as the address of the car2, so we will comment this
+        // Car5 car2 = new Car5("Car2", "Trucks", 2001);
+        // // car1 = car2; // the address of the car1 is the same as the address of the car2, so we will comment this
+        // car2.copy(car1); // it works as expected
+
+        Car5 car2 = new Car5(car1); // do the same thing as above
 
         System.out.println(car1);
         System.out.println(car2);
@@ -30,7 +33,11 @@ class Car5 {
         this.setMake(make);
         this.setModel(model);
         this.setYear(year);
+    }
 
+    // overloaded constructor
+    Car5(Car5 carx){
+        this.copy(carx);
     }
 
 
@@ -56,5 +63,14 @@ class Car5 {
     public void setYear(int year){
         this.year = year;
     }
+
+    // copy method
+    public void copy(Car5 carx){
+        this.setMake(carx.getMake());
+        this.setModel(carx.getModel());
+        this.setYear(carx.getYear());
+    }
+
+    
 
 }
--- Temp.java
+++ Temp.java
@@ -6,12 +6,18 @@ public class Temp {
         Scanner jin = new Scanner(System.in);
         n = jin.nextLong();
         System.out.println((n+1)*n/2);
-        if(n == 0){
-            
-        }    
+        
     }
 }
 
 class Holder{
-    
+    public int a1;
+    public int a2;
+    public int a3;
+
+    Holder(int a1, int a2, int a3) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
 }
